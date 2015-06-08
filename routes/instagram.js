a0316b225f8ad8c60bc934b922925173319e57b4
Ben Kauffman <Ben@172-16-10-52.dynapool.nyu.edu>
2015-06-08 15:48:17 -0400

    testing dupes with less often hashtag

--- routes/instagram.js
+++ routes/instagram.js
@@ -26,92 +26,92 @@ exports.map = function(req,res){
 /*
 	GET /instagram
 */
-exports.instagram = function(req, res){
+// exports.instagram = function(req, res){
 
-	Instagram.tags.recent({
-		name: '',
-		complete: function(data, pagination) {
-			res.json(buildGeoJson(data));
-	  	}
-	});
-};
+// 	Instagram.tags.recent({
+// 		name: '',
+// 		complete: function(data, pagination) {
+// 			res.json(buildGeoJson(data));
+// 	  	}
+// 	});
+// };
 
-function harvester() {
+// // function harvester() {
 
-	Instagram.tags.recent({
-		name: '',
-		complete: function(data,pagination) {
-			//regex strips the underscore and additional numbers from the ID that comes back
+// // 	Instagram.tags.recent({
+// // 		name: '',
+// // 		complete: function(data,pagination) {
+// // 			//regex strips the underscore and additional numbers from the ID that comes back
 			
-			var page = pagination;
-			console.log(page)
-			console.log()
-			for(each in data) {
-				if(data[each].location != null){
-					var regex = /^[^_]+(?=_)/g;
-					var dbDocument = {
-						id : regex.exec(String(data[each].id))[0],
-						coordinates : [ data[each].location.longitude, data[each].location.latitude ],
-						img_hi_res : data[each].images.standard_resolution.url,
-						img_lo_res : data[each].images.low_resolution.url,
-						img_thumb : data[each].images.thumbnail.url,
-						time : data[each].created_time
-					};
-				};
+// // 			var page = pagination;
+// // 			console.log(page)
+// // 			console.log()
+// // 			for(each in data) {
+// // 				if(data[each].location != null){
+// // 					var regex = /^[^_]+(?=_)/g;
+// // 					var dbDocument = {
+// // 						id : regex.exec(String(data[each].id))[0],
+// // 						coordinates : [ data[each].location.longitude, data[each].location.latitude ],
+// // 						img_hi_res : data[each].images.standard_resolution.url,
+// // 						img_lo_res : data[each].images.low_resolution.url,
+// // 						img_thumb : data[each].images.thumbnail.url,
+// // 						time : data[each].created_time
+// // 					};
+// // 				};
 
 
-				//console.log(dbDocument)
+// // 				//console.log(dbDocument)
 				
-				var insta = new instagramModel(dbDocument); // new db document
-				 //save to database
-				insta.save(function(err){
-					if(err) { console.log(err) }
-					else { console.log("saved harvested photos to database") }
-				});
-			};	
-		}
-	});
-};
+// // 				var insta = new instagramModel(dbDocument); // new db document
+// // 				 //save to database
+// // 				insta.save(function(err){
+// // 					if(err) { console.log(err) }
+// // 					else { console.log("saved harvested photos to database") }
+// // 				});
+// // 			};	
+// // 		}
+// // 	});
+// // };
 
-function getNewest(id) {
+// // function getNewest(id) {
 
-	Instagram.tags.recent({
-		name: '',
-		MAX_TAG_ID: id,
-		complete: function(data,pagination) {
+// // 	Instagram.tags.recent({
+// // 		name: '',
+// // 		MAX_TAG_ID: id,
+// // 		complete: function(data,pagination) {
 			
 			
-			var page = pagination;
-			//console.log(page)
-			//console.log(data)
-			for(each in data) {
-				if(data[each].location != null){
-					console.log(data[each].id);
-					//regex strips some non-relevant stuff from the ID that comes back
-					var regex = /^[^_]+(?=_)/g;
-					var dbDocument = {
-						id : regex.exec(String(data[each].id))[0],
-						coordinates : [ data[each].location.longitude, data[each].location.latitude ],
-						img_hi_res : data[each].images.standard_resolution.url,
-						img_lo_res : data[each].images.low_resolution.url,
-						img_thumb : data[each].images.thumbnail.url,
-						time : data[each].created_time
-					};
-				};
+// // 			var page = pagination;
+// // 			//console.log(page)
+// // 			//console.log(data)
+// // 			for(each in data) {
+// // 				if(data[each].location != null){
+// // 					console.log(data[each].id);
+// // 					//regex strips some non-relevant stuff from the ID that comes back
+// // 					var regex = /^[^_]+(?=_)/g;
+// // 					var dbDocument = {
+// // 						id : regex.exec(String(data[each].id))[0],
+// // 						coordinates : [ data[each].location.longitude, data[each].location.latitude ],
+// // 						img_hi_res : data[each].images.standard_resolution.url,
+// // 						img_lo_res : data[each].images.low_resolution.url,
+// // 						img_thumb : data[each].images.thumbnail.url,
+// // 						time : data[each].created_time
+// // 					};
+// // 				};
 
 
-				//console.log(dbDocument.id)
+// // 				//console.log(dbDocument.id)
 				
-				var insta = new instagramModel(dbDocument); // new db document
-				 //save to database
-				insta.save(function(err){
-					if(err) { console.log(err) }
-					else { console.log("saved "+ dbDocument.id + " to database" ) }
-				});
-			};	
-		}
-	});
-};
+// // 				var insta = new instagramModel(dbDocument); // new db document
+// // 				 //save to database
+// // 				insta.save(function(err){
+// // 					if(err) { console.log(err) }
+// // 					else { console.log("saved "+ dbDocument.id + " to database" ) }
+// // 				});
+// // 			};	
+// // 		}
+// // 	});
+// // };
 
 
 
